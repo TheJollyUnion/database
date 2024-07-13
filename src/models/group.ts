d23b763e6e59d23d01c79ef6348b0a29dbba80e8
Snowley <snowl3y@proton.me>
2024-07-13 00:26:28 -0600

    init commit

---
+++ src/models/group.ts
@@ -0,0 +1,20 @@
+import { template } from "lodash";
+import { Schema } from "ottoman";
+
+export const GroupSchema = new Schema({
+  id: { type: String, required: true },
+  template: { type: String, required: true },
+  inviteLink: { type: String, required: true },
+  status: { type: String, required: true },
+  clean: { type: Boolean, required: true },
+});
+
+GroupSchema.index.findByTid = { by: "_id", type: "n1ql" };
+
+export interface Group {
+  id: string;
+  template: string;
+  inviteLink: string;
+  status: "ready" | "loading" | "error";
+  clean: boolean;
+}
---
+++ src/tools/import.ts
@@ -0,0 +1,42 @@
+import { Groups, Templates } from "../index";
+import { readFile } from "fs/promises";
+import type { Group, Template } from "../models";
+
+const templateFile = await readFile("./templates.json", "utf8");
+const templates = JSON.parse(templateFile) as Template[];
+
+const groupFile = await readFile("./groups.json", "utf8");
+const groups = JSON.parse(groupFile) as Record<
+  string,
+  Omit<Group, "template">[]
+>;
+
+for (const templateCode in groups) {
+  const templateGroups = groups[templateCode];
+  for (const group of templateGroups) {
+    let existsInDB = false;
+    try {
+      existsInDB = await Groups.findOne({
+        id: group.id,
+      });
+    } catch (e) {}
+
+    if (existsInDB) continue;
+    group["template"] = templateCode;
+
+    await Groups.create(group as Group);
+  }
+}
+
+for (const template of templates) {
+  const templateCode = template.code;
+  let existsInDB = false;
+  try {
+    existsInDB = await Templates.findOne({
+      code: templateCode,
+    });
+  } catch (e) {}
+
+  if (existsInDB) continue;
+  await Templates.create(template as Template);
+}
